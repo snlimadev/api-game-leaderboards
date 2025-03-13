cd12afce331671b8127950f5712a57456e34799d
snlimadev <snlimadev@gmail.com>
2025-03-13 16:52:51 -0300

    Changing date format

--- api/utils/get_current_datetime.js
+++ api/utils/get_current_datetime.js
@@ -5,7 +5,7 @@ function getCurrentDatetime() {
     timeZone: 'America/Sao_Paulo',
     day: '2-digit',
     month: '2-digit',
-    year: '2-digit',
+    year: 'numeric',
     hour: '2-digit',
     minute: '2-digit',
     hour12: false
